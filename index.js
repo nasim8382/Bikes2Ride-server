d003a2bda40802a4f6a5cf4630cada0dec869fd7
Nasim Reza <nasim8382@gmail.com>
2022-05-05 18:15:17 +0600

    mongo db is connected with server side

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rmhxr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("bikes2Ride").collection("product");
+  // perform actions on the collection object
+  console.log('Mongo is connected');
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Bikes2Ride server is running successfully')
