f0365428fb767f463896d1f1468799e8dd1144c7
Nasim Reza <nasim8382@gmail.com>
2022-05-05 18:58:32 +0600

    data added in database and get from that

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -10,13 +11,24 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rmhxr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("bikes2Ride").collection("product");
-  // perform actions on the collection object
-  console.log('Mongo is connected');
-  client.close();
-});
 
+const run = async() => {
+    try {
+        await client.connect();
+        const productCollection = client.db("bikes2Ride").collection("product");
+
+        app.get('/product', async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Bikes2Ride server is running successfully')
