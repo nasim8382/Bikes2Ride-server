17572f9be5c3a3360d62745f7b3f79e2ad714674
Nasim Reza <nasim8382@gmail.com>
2022-05-08 12:43:35 +0600

    jwt implimented

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyToken (req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err) {
+            return res.status(403).send({message: 'Forbidden access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rmhxr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -17,6 +33,16 @@ const run = async() => {
         await client.connect();
         const productCollection = client.db("bikes2Ride").collection("product");
 
+        // Auth
+        app.post('/signin', async(req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({accessToken});
+        })
+
+        // product api
         app.get('/product', async(req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
@@ -35,6 +61,21 @@ const run = async() => {
             const product = await productCollection.findOne(query);
             res.send(product);
         });
+
+        // user products
+        app.get('/userproducts', verifyToken, async(req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if(email === decodedEmail) {
+                const query = {email: email};
+                const cursor = productCollection.find(query);
+                const items = await cursor.toArray();
+                res.send(items);
+            }
+            else{
+                res.status(403).send({message: 'Forbidden access'});
+            }
+        })
 
         // update quantity
         app.put('/product/:id', async(req, res) => {
